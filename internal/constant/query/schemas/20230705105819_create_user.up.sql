d6e09244676da8c895587fcd010244a60b7bdf37
alazarbeyene <alazarbeyene99@gmail.com>
2023-07-05 07:21:11 -0400

    user database schema created

---
+++ internal/constant/query/schemas/20230705105819_create_user.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users
+(
+  id   UUID  DEFAULT gen_random_uuid(), 
+  first_name  VARCHAR  ,
+  middle_name VARCHAR  ,
+  last_name   VARCHAR  ,
+  phone  VARCHAR  NULL,
+  profile     VARCHAR  ,
+  status status NOT NULL,
+  created_at  TIMESTAMP NOT NULL DEFAULT now(),
+  updated_at  TIMESTAMP,
+  deleted_at  TIMESTAMP,
+  PRIMARY KEY (id)
+);
