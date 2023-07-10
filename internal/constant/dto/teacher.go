c568698474b22a51732c61dbabbb355553a0f0aa
alazarbeyene <alazarbeyene99@gmail.com>
2023-07-10 02:45:36 -0400

    done working on module layer of create teacher

--- internal/constant/dto/teacher.go
+++ internal/constant/dto/teacher.go
@@ -1,17 +1,28 @@
 package dto
 
 import (
+	"fmt"
 	"schoolcms/internal/constant/model/db"
 
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/google/uuid"
 )
 
 type Teacher struct {
-	ID         uuid.UUID `json:"id"`
-	UserId     uuid.UUID `json:"user_id"`
-	FirstName  string    `json:"first_name"`
-	MiddleName string    `json:"middle_name"`
-	LastName   string    `json:"last_name"`
-	Title      string    `json:"title"`
-	Status     db.Status `json:"status"`
+	ID     uuid.UUID `json:"id"`
+	UserId uuid.UUID `json:"user_id"`
+	Title  string    `json:"title"`
+	Status db.Status `json:"status"`
+}
+
+func (ts Teacher) ValidteTeachers() error {
+	return validation.ValidateStruct(&ts,
+		validation.Field(&ts.UserId, validation.By(func(value interface{}) error {
+			if ts.UserId == uuid.Nil {
+				return fmt.Errorf("invalid user id, user id required")
+			}
+			return nil
+		})),
+		validation.Field(&ts.Title, validation.Required.Error("title required")),
+	)
 }
